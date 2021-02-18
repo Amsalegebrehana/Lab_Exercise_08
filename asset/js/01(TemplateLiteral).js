b66f4334c2c549c9e07a040d697ddcaecb7b588a
Amsale Gebrehana <se.amsale.gebrehana@gmail.com>
2021-02-18 15:49:36 +0300

    working on the questions

---
+++ asset/js/01(TemplateLiteral).js
@@ -0,0 +1,20 @@
+//DOM
+document.addEventListener('DOMContentLoaded', () =>{
+    templateStringCocatenation();
+    displayTemplateExpression();
+})
+
+function templateStringConcatenation(){
+
+    // A Student Object
+    const student = {name: "Abebe", age: 21}
+
+    //String to Display
+    const fullIntro = "My name is " + student.name + "and I am " + student.age + "Years Old"
+
+    //1. Create a variable called fullIntroTemplate and Store the same string displayed above with template literal
+    
+    var fullIntroTemplate
+
+
+}
